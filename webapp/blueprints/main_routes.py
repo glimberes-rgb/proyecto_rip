14bc39c3504b7a5e4c4a45aec51fca864ed6753c
glimberes-rgb <glimber.es@tecnologicoargos.edu.ec>
2026-07-08 09:53:22 -0500

    Delete webapp/blueprints directory

--- webapp/blueprints/main_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-from . import main_bp
-
-@main_bp.route('/')
-def index():
-    """Renderiza la página web principal con el contenedor del mapa Leaflet"""
-    return render_template('index.html')
